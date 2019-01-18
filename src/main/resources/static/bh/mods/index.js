4145b575f528f3b970d340638f71e2e2c0bddf0a
cold <zjchenliang@126.com>
2019-01-18 19:48:28 +0800

    提交代码

--- src/main/resources/static/bh/mods/index.js
+++ src/main/resources/static/bh/mods/index.js
@@ -4,4 +4,15 @@
         ,form = layui.form;
 
     layer.msg('Hello World');
+
+
+    var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
+
+    //监听导航点击
+    element.on('nav(demo)', function(elem){
+        //console.log(elem)
+        layer.msg(elem.text());
+    });
+
+
 }();
