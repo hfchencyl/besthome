466a58777410e4370afd0d540dadb78e19135bcf
cold <zjchenliang@126.com>
2019-02-01 15:10:19 +0800

    打底的数据提交下

--- src/main/java/com/besthome/besthome/web/index/IndexController.java
+++ src/main/java/com/besthome/besthome/web/index/IndexController.java
@@ -4,11 +4,16 @@ import com.besthome.besthome.web.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class IndexController extends BaseController {
 
     @RequestMapping("/index")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        HttpSession session = request.getSession();//获取session并将userName存入session对象
+        session.setAttribute("userName", "chenliang");
         return "index";
     }
 }
